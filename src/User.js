b51edafa6dd69560966dac205981fb7afed43fb5
Justin Berg <justin.berg1@verizon.com>
2024-09-13 13:48:12 -0500

    finished the logic for user

--- src/User.js
+++ src/User.js
@@ -1,5 +1,26 @@
 class User {
-  // User code here
+  constructor(username, password, age,) {
+    this.username = username;
+    this.password = password;
+    this.age = age;
+    this.loggedIn = false;
+  }
+  login(password) {
+  if (this.password === password) {
+    this.loggedIn = true;
+    console.log(`${this.username} is logged in`)
+  } else {
+    throw new Error('Incorrect Password')
+  }
+}
+logout() {
+  if (this.loggedIn) {
+    this.loggedIn = false;
+    console.log(`${this.username} has logged out`);
+  } else {
+    throw new Error('User is not logged in')
+  }
+}
 }
 
 module.exports = User
